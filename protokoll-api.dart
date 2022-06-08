be3392b7b8280d71fb6db770049a966d9df88ed9
Miicrop <103122613+Miicrop@users.noreply.github.com>
2022-06-08 11:04:01 +0200

    Delete protokoll-api.dart

--- protokoll-api.dart
+++
@@ -1,8 +0,0 @@
-import 'protokoll.dart';
-import 'package:http/http.dart' as http;
-
-Future<List<Protokoll>> fetchProtokoll() async {
-  String url = 'http://192.168.0.194/RFID_Website/app.php';
-  final response = await http.get(Uri.parse(url));
-  return protokollFromJson(response.body);
-}
